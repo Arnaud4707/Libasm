dcb1ceee1c680b738fa322e92b3e028f6d9cbe34
Arnaud Mugisha <amugisha6@gmail.com>
2025-10-23 19:48:03 +0200

    change the logic's systeme stack

--- src/ft_list_remove_if.s
+++ src/ft_list_remove_if.s
@@ -9,7 +9,7 @@ section .bss
 section .data
     msg_debug db "stop", 10
     len_debug equ $ - msg_debug
-    msg_printf db "%p | %p", 10
+    msg_printf db "pre -> %p | curr -> %p", 10
     len_debug_printf equ $ - msg_printf
 
 section .text
@@ -18,88 +18,74 @@ section .text
     extern printf
 
 ft_list_remove_if:
-    push    rbp
-    mov     rbp, rsp
-
-    push    rbx             ; callee-saved
-    push    r12
-    push    r13
-
-    mov rbx, rdi
-    xor r13, r13
-    mov r12, [rbx]
-    mov r14, rsi
-    mov r15, rdx
+    push rbp
+    mov rbp, rsp
+	sub	rsp, 64
+	mov [rsp + 0], rdi ; heap
+	mov	rbx, [rsp + 0]
+	mov rax, [rbx]
+	xor r8, r8
+	mov [rsp + 8], rsi
+	mov [rsp + 16], rdx ; func
+	mov [rsp + 24], r8 ; prev
+	mov [rsp + 32], rax ; curr
 
 .loop:
-    test r12, r12
-    je .done
-    mov rdi, [r12 + t_list.data]
-    mov rsi, r14
-    sub rsp, 8
-	call r15
-    add rsp, 8
-    test rax, rax
-    je .replace
-    mov r13, r12
-    mov r9, [r12 + t_list.next]
-    mov r12, r9
-    jmp .loop
+	test rax, rax
+	je .done
+	mov rdi, [rax + t_list.data]
+	mov rsi, [rsp + 8]
+	mov r8, [rsp + 16]
+	sub rsp, 8
+	call r8
+	add rsp, 8
+	test rax, rax
+	je .replace
+	mov r8, [rsp + 32]
+	mov [rsp + 24], r8
+	mov rax, [r8 + t_list.next]
+	mov [rsp + 32], rax
+	jmp .loop
 
 .replace:
-    test r13, r13
-    je .replace_head
-    mov rdi, r12
-    mov r9, [r12 + t_list.next]
-    xor r12, r12
-	mov r12, r9
-    sub rsp, 8
+	mov rax, [rsp + 24]
+	test rax, rax
+	je .replace_head
+	mov rdi, [rsp + 32]
+	mov r10, [rdi + t_list.next]
+	sub rsp, 8
 	call ft_free
-    xor rdi, rdi
-    add rsp, 8
-    mov [r13 + t_list.next], r12
+	add rsp, 8
+	mov [rsp + 32], r10
+	mov rax, [rsp + 24]
+	mov [rax + t_list.next], r10
+	mov rax, r10
 	jmp .loop
 
 .replace_head:
-    ; call .debug_printf
-    push rbx
-    mov rdi, r12
-    mov r12, [r12 + t_list.next]
-	mov [rbx], r12
-    sub rsp, 8
+	mov rdi, [rsp + 32]
+	mov r10, [rdi + t_list.next]
+	sub rsp, 8
 	call ft_free
-    xor rdi, rdi
-    add rsp, 8
-    xor r13, r13
-    pop rbx
-    jmp .loop
-
-.done:
-    pop     r13
-    pop     r12
-    pop     rbx
-    mov rsp, rbp
-    pop rbp
-    ret
+	add rsp, 8
+	mov [rsp + 32], r10
+	mov rbx, [rsp + 0]
+	mov [rbx], r10
+	mov rax, r10
+	jmp .loop
 
 .debug_printf:
-    ; sub rsp, 8 
     mov rdi, msg_printf
-    mov rsi, r12
-    mov rdx, rbx
-    call printf
-    ; add rsp, 8
+    mov rsi, [rsp + 24]
+    mov rdx, [rsp + 32]
+	push rdx
+	push rsi
+	call printf
+	pop rsi
+	pop rdi
     ret
 
-.debug:
-    mov rax, 1
-    mov rdi, 2
-    mov rsi, msg_debug
-    mov rdx, len_debug
-    syscall
-    ret
-    ; mov rax, 60
-    ; mov rdi, 4
-    ; syscall
-    ; jmp .done
-
+.done:
+	mov rsp, rbp
+	pop rbp
+	ret
